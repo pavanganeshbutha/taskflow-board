8d12a04653f57bab9570fdd2e34f7ce91b886bb1
pavanganeshbutha <pavanganeshbutha@gmail.com>
2026-04-30 22:08:24 +0530

    feat: implement LocalStorage serialization and OOP state rehydration

--- scripts/app.js
+++ scripts/app.js
@@ -47,6 +47,22 @@ class Board {
   }
 
   initializeColumns() {
+    const taskflow_data = localStorage.getItem("taskflow_data");
+    if (taskflow_data) {
+      const parsedColumns = JSON.parse(taskflow_data);
+      this.columns = [];
+      parsedColumns.forEach((column) => {
+        const realColumn = new Column(column.id, column.title);
+        column.tasks.forEach((task) => {
+          const realTask = new Task(task.name, task.description, task.priority);
+          realTask.id = task.id;
+          realTask.status = task.status;
+          realColumn.addTask(realTask);
+        });
+        this.columns.push(realColumn);
+      });
+      return;
+    }
     const todoColumn = new Column("col-todo", "To Do");
     const inProgressColumn = new Column("col-in-progress", "In Progress");
     const doneColumn = new Column("col-done", "Done");
@@ -72,8 +88,14 @@ class Board {
         const initialTask = new Task("Fix Header", "Align the logo", "High");
         column.addTask(initialTask);
         this.render();
+        this.saveState();
       }
     });
+  }
+
+  saveState() {
+    const stringifyColumns = JSON.stringify(this.columns);
+    localStorage.setItem("taskflow_data", stringifyColumns);
   }
 }
 
